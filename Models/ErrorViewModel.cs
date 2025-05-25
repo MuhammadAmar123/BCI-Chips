e0cf7e0973d68b69c08bc573364a707a07789572
Muhammad Amar <ac116681@avcol.school.nz>
2025-05-25 23:13:02 +1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BCI_Chips.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
