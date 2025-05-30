595aaf70423ca951f5476dba0215dc192e90f21c
Muhammad Amar <ac116681@avcol.school.nz>
2025-05-30 23:04:08 +1200

    added remaining pages and dropdown menu

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,34 @@ namespace BCI_Chips.Controllers
         {
             return View();
         }
+        public IActionResult Opportunities()
+        {
+            return View();
+        }
+        public IActionResult Risks()
+        {
+            return View();
+        }
+        public IActionResult References()
+        {
+            return View();
+        }
+        public IActionResult Choices()
+        {
+            return View();
+        }
+        public IActionResult GuidelineConform()
+        {
+            return View();
+        }
+        public IActionResult MeetingMinutes()
+        {
+            return View();
+        }
+        public IActionResult TeamFormation()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
